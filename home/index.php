3f2daaa248c51f0c09f7fbc6026d506504c13f96
sheylaprl <sheylaaprilia2@gmail.com>
2024-05-06 22:20:11 +0800

    file rapi,login dan register

---
+++ home/index.php
@@ -0,0 +1,167 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Home</title>
+    <link rel="stylesheet" href="../asset/style/project1global.css">
+    <link rel="stylesheet" href="../asset/style/home.css">
+    <link rel="stylesheet" href="carousel_card.css">
+
+    <script src="carousel_card.js"></script>
+</head>
+
+<body>
+    <header>
+        <div style="display: flex; gap: 18px;">
+            <img src="../asset/icon/menu.svg" width="30px" height="30px">
+            <img src="../asset/icon/search.svg" width="30px" height="30px">
+        </div>
+        <img src="../asset/icon/icon.svg" width="30px" height="30px">
+        <div style="display: flex; gap: 18px;">
+            <img src="../asset/icon/user.svg" width="30px" height="30px">
+            <img src="../asset/icon/wishlist.svg" width="30px" height="30px">
+            <img src="../asset/icon/basket.svg" width="30px" height="30px">
+        </div>    
+    </header>
+
+    <div class="home">
+        <h1>femme fatale</h1>
+        <img src="../asset/image/homepic.png" style="width: 500px; height: 500px;">
+    </div>
+    <div class="aboutus">
+        <img src="../asset/image/quoteimg.png" style="width: 500px; height: 500px;">
+        <h2 style="text-align: center; "> She knows her worth and doesn’t 
+            need to prove anything to
+            anyone</h2>
+    </div>
+
+    <section>
+        <div id="cCarousel">
+          <div class="arrow" id="prev"><i class="fa-solid fa-chevron-left"></i></div>
+          <div class="arrow" id="next"><i class="fa-solid fa-chevron-right"></i></div>
+      
+          <div id="carousel-vp">
+            <div id="cCarousel-inner">
+      
+              <article class="cCarousel-item">
+                <img src="https://images.unsplash.com/photo-1564292284209-60cce69f08ed?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2ODQ0MTA1MTJ8&ixlib=rb-4.0.3&q=80&w=400" alt="Moon">
+                <div class="infos">
+                  <h3>Title 1 / 6</h3>
+                  <button type="button">See</button>
+                </div>
+              </article>
+      
+              <article class="cCarousel-item">
+                <img src="https://images.unsplash.com/photo-1564292284209-60cce69f08ed?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2ODQ0MTA1MTJ8&ixlib=rb-4.0.3&q=80&w=400" alt="Moon">
+                <div class="infos">
+                  <h3>Title 2 / 6</h3>
+                  <button type="button">See</button>
+                </div>
+              </article>
+      
+              <article class="cCarousel-item">
+                <img src="https://images.unsplash.com/photo-1564292284209-60cce69f08ed?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2ODQ0MTA1MTJ8&ixlib=rb-4.0.3&q=80&w=400" alt="Moon">
+                <div class="infos">
+                  <h3>Title 3 / 6</h3>
+                  <button type="button">See</button>
+                </div>
+              </article>
+      
+              <article class="cCarousel-item">
+                <img src="https://images.unsplash.com/photo-1564292284209-60cce69f08ed?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2ODQ0MTA1MTJ8&ixlib=rb-4.0.3&q=80&w=400" alt="Moon">
+                <div class="infos">
+                  <h3>Title 4 / 6</h3>
+                  <button type="button">See</button>
+                </div>
+              </article>
+      
+              <article class="cCarousel-item">
+                <img src="https://images.unsplash.com/photo-1564292284209-60cce69f08ed?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2ODQ0MTA1MTJ8&ixlib=rb-4.0.3&q=80&w=400" alt="Moon">
+                <div class="infos">
+                  <h3>Title 5 / 6</h3>
+                  <button type="button">See</button>
+                </div>
+              </article>
+      
+              <article class="cCarousel-item">
+                <img src="https://images.unsplash.com/photo-1564292284209-60cce69f08ed?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2ODQ0MTA1MTJ8&ixlib=rb-4.0.3&q=80&w=400" alt="Moon">
+                <div class="infos">
+                  <h3>Title 6 / 6</h3>
+                  <button type="button">See</button>
+                </div>
+              </article>
+            </div>
+          </div>
+        </div>
+      </section>
+
+    <div id="newarrival">
+        <h1 style="font-size: 60px;"> New Arrival! </h1>
+        <div style="display: flex; flex-direction: row; align-items: center; gap: 12px;">
+            <div>
+                <img src="../asset/icon/panahleft.svg" alt="">
+            </div>
+            <div class="item">
+                <div>
+                    <img src="../asset/image/dress claudia.png" style="width: 300px; height: 300px;">
+                    <div style="display: flex; flex-direction: column; align-items: center; gap: 12px;">
+                        <h3>Claudia Dress
+                            <h3>$259.98</h3>
+                        </h3>
+                    </div>
+                </div>
+                <div>
+                    <img src="../asset/image/latulipe necklace.png" style="width: 300px; height: 300px;">
+                    <div style="display: flex; flex-direction: column; align-items: center; gap: 12px;">
+                        <h3>Latulipe Necklace
+                            <h3>$259.98</h3>
+                        </h3>
+                    </div>
+                </div>
+                <div>
+                    <img src="../asset/image/arabella dress.png" style="width: 300px; height: 300px;">
+                    <div style="display: flex; flex-direction: column; align-items: center; gap: 12px; ">
+                        <h3>Arabella Dress
+                            <h3>$259.98</h3>
+                        </h3>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <img src="../asset/icon/panahright.svg" alt="">
+            </div>
+        </div>
+    </div>
+
+    <div>
+        <div class="itemclick">
+            <img src="../asset/image/dresspic.png" alt="">
+            <h1> DRESS COLLECTION</h1>
+        </div>
+        <div class="itemclick">
+            <h1> ACCESSORIES </h1>
+            <img src="../asset/image/necklacepic.png" alt="">
+        
+        </div>
+        <div class="itemclick">
+            <img src="../asset/image/bagpic.png" alt="">
+            <h1>BAGS </h1>
+        </div>
+  
+    </div>
+    
+    <footer >
+     <div style="display: flex; flex-direction: column; gap: 18px; width: 254px; height: 70px;">
+        <div style="display: flex; gap: 18px;">
+            <img src="yt.svg" alt="">
+            <img src="twitter.svg" alt="">
+            <img src="ig.svg" alt="">
+            <img src="fb.svg" alt="">
+        </div>
+    
+        <h4 style="color: #E31212;">© 2024 Femme Fatale Store. All Rights Reserved  | Help | Terms | Privacy</h4>
+     </div>
+
+     </footer>
+
+</body>
+</html>
